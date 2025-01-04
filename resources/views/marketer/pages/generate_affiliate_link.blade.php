e55930375e615d628633d467b740e37fab6afc1a
Tajim Islam <tjm4181@gmail.com>
2025-01-04 22:18:00 +0600

    campaigns

--- resources/views/marketer/pages/generate_affiliate_link.blade.php
+++ resources/views/marketer/pages/generate_affiliate_link.blade.php
@@ -14,9 +14,7 @@
 <style>
 .copy-link {
   --height: 36px;
-
   display: flex;
-  max-width: 250px;
 }
 
 .copy-link-input {
@@ -34,7 +32,6 @@
 
 .copy-link-button {
   flex-shrink: 0;
-  width: var(--height);
   height: var(--height);
   display: flex;
   align-items: center;
@@ -59,19 +56,17 @@
     <div class="card">
         <div class="card-header">
             <h4><b>@lang('static.marketer.generate_affiliate_link')</b></h4>
-            <div class="btn-action">
-                <div class="row">
-                    <div class="col-6">
-                        <h4 style="color: #009417;"><b>৳ 230.32</b></h4>
-                        <span>My Affiliate Earnings (Last 30 days)</span>
-                    </div>
-                    <div class="col-6">
-                        <h4 style="color:rgb(243, 13, 9);"><b>৳ 510.32</b></h4>
-                        <span>Affiliate Wallet Balance</span>
-                    </div>
+            <div class="row">
+                <div class="col-6">
+                    <h4 style="color: #009417;"><b>৳ 230.32</b></h4>
+                    <span>My Affiliate Earnings (Last 30 days)</span>
+                </div>
+                <div class="col-6">
+                    <h4 style="color:rgb(243, 13, 9);"><b>৳ 510.32</b></h4>
+                    <span>Affiliate Wallet Balance</span>
                 </div>
             </div>
-            <div class="col-12 d-flex align-items-center gap-sm-3 gap-2 p-0">
+            <div class="col-12 d-flex gap-sm-3 gap-2 mt-5 align-items-center">
                 <img class="active-icon" src="{{ asset('frontend/images/svg/send.svg') }}">
                 <h6> Default Affiliate Link: </h6>
                 <div class="copy-link">
@@ -81,14 +76,12 @@
                     </button>
                 </div>
             </div>
-            <div class="col-12 gap-sm-3 gap-2 p-3">
-                <h6 style="margin-left: 60px;"><b>Affiliate Link Generator</b></h6>
+            <div class="col-12 gap-sm-3 gap-2 p-3 mt-5">
+                <h5 style="margin-left: 44px;">Affiliate Link Generator</h5>
                 <p class="p-3">If you'd like to add your own affiliate links with a different URL, follow this structure. Enter any URL from this website to generate a referral link.</p>
             </div>
-        </div>
-        <div class="align-items-center p-0" style="width: 63%;margin-left: 35px;">
-            <div class="row">
-                <div class="col-5 mt-2">
+            <div class="col-12 d-flex gap-sm-3 gap-2 p-3">
+                <div class="col-4 mt-2">
                     <label for="" class="form-label">Campaign:</label>
                     <select name="" id="" class="form-control bg-white"> 
                         <option value="">Cleaning Service campaign</option>
@@ -99,85 +92,72 @@
                     <input type="text" class="form-control bg-white">
                 </div>
             </div>
-        </div>
-        <div class="d-flex gap-sm-3 gap-2 p-3" style="margin-left: 35px;">
-            <button class="btn btn-danger" style="width: 27%;">Generate Link</button>
-            <img class="active-icon" src="{{ asset('frontend/images/svg/send.svg') }}" style="margin-left: 13px;">
-            <div class="copy-link">
-                <input type="text" class="copy-link-input" value="https://aso.com.bd/category/cleaning/?ref=1" readonly>
-                <button type="button" class="copy-link-button">
-                    <img class="active-icon" src="{{ asset('frontend/images/svg/copy.svg') }}">
-                </button>
+            <div class="col-12 d-flex align-items-center gap-sm-3 gap-2 p-3">
+                <div class="col-4">
+                    <button class="btn btn-warning w-100">Generate Link</button>
+                </div>
+                <div class="col-7 d-flex">
+                    <img class="active-icon p-1" src="{{ asset('frontend/images/svg/send.svg') }}">
+                    <div class="copy-link" style="width: 100% !important;">
+                        <input type="text" class="copy-link-input" value="https://aso.com.bd/category/cleaning/?ref=1" readonly>
+                        <button type="button" class="copy-link-button">
+                            <img class="active-icon" src="{{ asset('frontend/images/svg/copy.svg') }}">
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row mt-5 mb-5">
-            <div class="col-xxl-4 col-xl-12 col-12">
-                <a href="{{ route('backend.provider.index') }}" class="widget-card card">
+        <div class="row mt-5 mb-5 p-3">
+            <div class="col-md-4 col-12">
+                <a href="{{ route('backend.provider.index') }}" class="widget-card">
                     <div>
                         <h3>5.89%</h3>
-                        <h5>Conversion Rate</h5>
+                        <h5><b>Conversion Rate</b></h5>
                     </div>
                 </a>
             </div>
-            <div class="col-xxl-3 col-xl-12 col-12">
-                <a href="{{ route('backend.serviceman.index') }}" class="widget-card card">
+            <div class="col-md-3 col-12">
+                <a href="{{ route('backend.serviceman.index') }}" class="widget-card">
                     <div>
                         <h3>31,210</h3>
-                        <h5>Total Click</h5>
+                        <h5><b>Total Click</b></h5>
                     </div>
                 </a>
             </div>
-            <div class="col-xxl-3 col-xl-12 col-12">
-                <a href="{{ route('backend.serviceman.index') }}" class="widget-card card">
+            <div class="col-md-3 col-12">
+                <a href="{{ route('backend.serviceman.index') }}" class="widget-card">
                     <div>
                         <h3>৳ 10,520</h3>
-                        <h5>Total Order | 89</h5>
+                        <h5><b>Total Order | 89</b></h5>
                     </div>
                 </a>
             </div>
         </div>
-        <div class="card-body pb-0">
-            <table class="table table-striped table-responsive">
-                <thead>
-                    <tr>
-                        <th>Affiliate Link</th>
-                        <th>Campaign</th>
-                        <th>Created</th>
-                        <th>Visits</th>
-                        <th>Order</th>
-                        <th>Conversion</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                    </tr>
-                </tbody>
+        <div class="card-body p-3">
+            <div class="container table-responsive py-5"> 
+                <table class="table table-striped table-responsive">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Affiliate Link</th>
+                            <th>Campaign</th>
+                            <th>Created</th>
+                            <th>Visits</th>
+                            <th>Order</th>
+                            <th>Conversion</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>demotext</td>
+                            <td>demotext</td>
+                            <td>demotext</td>
+                            <td>demotext</td>
+                            <td>demotext</td>
+                            <td>demotext</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
